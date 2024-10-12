5ac7b6fe9ffb1466d64b55b5543f09af05ffa014
Ana Paula Endler <anapaulaendler@gmail.com>
2024-10-12 13:02:27 -0300

    alterando o modelo Atividade e adicionando o GET de Leitura

--- SolucaoSistemaPenal/API/Models/Atividade.cs
+++ SolucaoSistemaPenal/API/Models/Atividade.cs
@@ -9,10 +9,11 @@ public class Atividade
     Id = Guid.NewGuid().ToString();
     AnoAtual = DateTime.Now.Year;
   }
-  public string? Id { get; set; }
+  public string Id { get; set; }
   public string? Nome { get; set; }
   public int Contador { get; set; }
   public float Equivalencia { get; set; }
+  // ana: float ou int?
   public int AnoAtual { get; set; }
-  public string? Limite { get; set; }
+  public int Limite { get; set; }
 }
