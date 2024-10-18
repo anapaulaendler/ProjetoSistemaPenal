ca6b39962b418ffcabc57e647053b6741be380aa
PedroWernek <142018686+PedroWernek@users.noreply.github.com>
2024-10-18 08:54:42 -0300

    Cadastrando atividade em detento ja cadastrado

--- SolucaoSistemaPenal/API/Models/Atividade.cs
+++ SolucaoSistemaPenal/API/Models/Atividade.cs
@@ -5,7 +5,5 @@ public abstract class Atividade
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string DetentoId { get; set; } = null!;
     public int Contador { get; set; }
-
-    public Atividade() {}
     
 }
