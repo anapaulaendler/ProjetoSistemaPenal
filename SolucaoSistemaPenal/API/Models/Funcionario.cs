d1acc7fb7eaacfac963c9fbb9096343cb457c0bf
Ana Paula Endler <anapaulaendler@gmail.com>
2024-10-31 09:52:06 -0300

    Criando a Entidade Funcionário

---
+++ SolucaoSistemaPenal/API/Models/Funcionario.cs
@@ -0,0 +1,14 @@
+namespace API.Models;
+
+public class Funcionario : Pessoa
+{
+    public string FuncionarioId { get; set; } = Guid.NewGuid().ToString();
+    public required string Cargo { get; set; } 
+    // o cargo precisa aparecer pra gente definir se o funcionário pode
+    // alterar a pena
+
+    // "Diretor(a) do Presídio" / "Agente Penitenciário Especializado" / "Assistente Social": PODE
+    // "Agente de Segurança Penitenciária (ASP)" / "Chefe de Vigilância e Disciplina" / "Inspetor Penitenciário" / 
+    // "Enfermeiro(a) Penitenciário(a)" / "Médico(a) Penitenciário(a)" / "Coordenador(a) de Educação e Trabalho" / 
+    // "Professor(a) de Educação Prisional" / "Nutricionista" / "Cozinheiro(a)" / "Assistente Administrativo": NAO PODE
+}
--- SolucaoSistemaPenal/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ SolucaoSistemaPenal/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2721135b76c3dcbd757e5554b56e47ad2f8b04c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e1a72f907bfde2aa2b2b31cd323692ff142163")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
