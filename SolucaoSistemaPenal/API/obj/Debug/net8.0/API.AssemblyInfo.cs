d69142017936d82b3b427de269d78d19293414ad
Ana Paula Endler <anapaulaendler@gmail.com>
2024-11-06 12:34:26 -0300

    ??

--- SolucaoSistemaPenal/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ SolucaoSistemaPenal/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d238777090405e545d8a2d65c902f33451148e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4fa7dc7d33cd6806eb320f8fc13d30bb15f5ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
